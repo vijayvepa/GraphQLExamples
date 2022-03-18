50ee94f501583f623df7b1cb60e17d1fa43a64ca
vijayvepakomma <vijay.vepakomma@philips.com>
2022-03-18 08:21:48 -0400

    add graphiql server

--- GraphQL In Action/ImplementResolvers/graphql-in-action/api/src/server.js
+++ GraphQL In Action/ImplementResolvers/graphql-in-action/api/src/server.js
@@ -3,18 +3,24 @@ import cors from 'cors';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import {Configuration} from "./configuration";
+import {graphqlHTTP} from "express-graphql";
+import {schema, rootValue} from "./schema";
 
-async function main(){
+async function main() {
 
     const server = express();
 
     server.use(cors());
     server.use(morgan('dev'));
-    server.use(bodyParser.urlencoded({extended:false}));
+    server.use(bodyParser.urlencoded({extended: false}));
     server.use(bodyParser.json());
-    server.use('/:fav.ico', (req,res) => res.sendStatus(204));
+    server.use('/:fav.ico', (req, res) => res.sendStatus(204));
 
-    server.use('/', (req,res)=> {res.send('Hello world!');});
+    server.use('/hello', (req, res) => {
+        res.send('Hello world!');
+    });
+
+    server.use('/', graphqlHTTP({schema, rootValue, graphiql: true}));
 
     server.listen(Configuration.PORT, () => {
         console.log(`Server URL: http://localhost:${Configuration.PORT}`);
