ca10f52f261dc4e5ceab8df47f180129f8caefaa
vijayvepakomma <vijay.vepakomma@philips.com>
2022-03-18 07:53:33 -0400

    cli example

--- GraphQL In Action/ImplementResolvers/graphql-in-action/api/src/cli.js
+++ GraphQL In Action/ImplementResolvers/graphql-in-action/api/src/cli.js
@@ -1,5 +1,5 @@
 import {graphql} from "graphql";
-import {rootValue, schema} from "./index";
+import {rootValue, schema} from "./schema";
 
 const executeGraphQLRequest = async request => {
     const resp = await graphql({schema: schema, source: request, rootValue: rootValue});
