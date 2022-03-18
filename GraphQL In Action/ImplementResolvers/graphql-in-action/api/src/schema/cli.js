385fd5ef13ca408ba00dde1451ace8c895ea674f
vijayvepakomma <vijay.vepakomma@philips.com>
2022-03-18 07:41:23 -0400

    cli example

---
+++ GraphQL In Action/ImplementResolvers/graphql-in-action/api/src/schema/cli.js
@@ -0,0 +1,12 @@
+import {graphql} from "graphql";
+import {rootValue, schema} from "./index";
+
+const executeGraphQLRequest = async request => {
+    const resp = await graphql({schema: schema, source: request, rootValue: rootValue});
+    console.log(resp.data);
+}
+
+console.log(process.argv[2]);
+executeGraphQLRequest(process.argv[2]);
+
+//node -r esm api/src/schema/cli.js "{ currentTime }"
